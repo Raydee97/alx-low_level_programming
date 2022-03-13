78afcf412df8f7e0bd2ccc88fcd04a24dafe1dfa
Raydee97 <blvckree@gmail.com>
2022-03-13 06:03:06 -0700

    FILES EXECUTED

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,24 +3,49 @@
 #include <stdio.h>
 
 /**
- * main- positive, negetive or zero
+ *main- positive, negative or zero
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-        int n;
+	int n;
 
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	        printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	else if (n < 0)
-	         printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	else
-	         printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	return (0);
 }
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                                                 1,1           All
+
