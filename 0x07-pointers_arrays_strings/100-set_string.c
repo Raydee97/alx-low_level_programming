1fc687b56876d3ecb8a777d9a23b3d437281e46e
Raydee97 <blvckree@gmail.com>
2022-03-28 10:51:10 -0700

    PAAS

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,12 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *  * set_string - sets the value of a pointer to a char
- *   * @s: source adress.
- *    * @to: target adress.
- *     * Return: no return.
- *      */
+ * set_string - sets the value of a pointer to a char
+ * @s: source adress.
+ * @to: target adress.
+ * Return: no return.
+ */
 void set_string(char **s, char *to)
 {
-		*s = to;
+	*s = to;
 }
